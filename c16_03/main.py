bc832b7c57139fa694694be9dc45df3084fd7857
chloe731 <chloe.landais@etu.u-paris.fr>
2021-03-24 17:47:56 +0000

    Rendu_contrôle

--- c16_03/main.py
+++ c16_03/main.py
@@ -7,38 +7,46 @@ note6 = ('eleve1', 'math', 12)
 note7 = ('eleve2', 'math', 13)
 note8 = ('eleve2', 'math', 14)
 
+
 notes = [note1, note2, note3, note4, note5, note6,note7,note8]
 
 #print(note1[0])
 
-# 4a
 
-moyenne1 = (notes[0][2] + notes[1][2] + notes[2][2] + notes[3][2] + notes[4][2] + notes[5][2])/6
-print("Moyenne élèvé 1 : ",moyenne1)
-
-# 4b
-moyenne1_math = (notes[0][2] + notes[2][2]+ notes[5][2])/3
-print("Moyenne élèvé 1 en mathématiques : ",moyenne1_math)
-
-#4c
-
-def moyenne_tuple(notes, eleve, matiere) :
-	s=0
-	i=0
-	for x in notes :
-		if x[0] == eleve and x[1] == matiere :
-			i = i + 1
-			s = s+x[2]
-		
-
-	moy = s / i 
-	return(moy)
+#moyenne1 = (notes[0][2] + notes[1][2] + notes[2][2] + notes[3][2] + notes[4][2] + notes[5][2])/6
+#print("Moyenne élèvé 1 : ",moyenne1)
 
 
+#moyenne1_math = (notes[0][2] + notes[2][2]+ notes[5][2])/3
+#print("Moyenne élèvé 1 en mathématiques : ",moyenne1_math)
+
+
+
+
+def moyenne_eleve1(liste):
+  moyenne = [ item[2]  for item in liste if  item[0]=="eleve1"]
+  moyenne1 = sum(moyenne)/len(moyenne)
+  return moyenne1
+print(moyenne_eleve1(notes))
+
+
+
+def moyenne_eleve1(liste):
+  moyenne = [ item[2]  for item in liste if  item[0]=="eleve1" and item[1]=="math" ]
+  moyenne1 = sum(moyenne)/len(moyenne)
+  return moyenne1
+print(moyenne_eleve1(notes))
+
+
+
+def moyenne_tuple(notes, eleve, matiere):
+  moyenne = [x for x in notes if x[0] == eleve]
+  moyenne_matiere = [x for x in moyenne if x[1] == matiere]
+  return sum([x[2] for x in moyenne_matiere ])/len(moyenne_matiere )
 
 m = moyenne_tuple(notes,'eleve1','math')
 print(m)
-
+  
 # Que se passe-t-il si on fait une faute de frappe dans la saisie d'une matière ? 
 # Le logiciel ne trouve pas de matière donc ne renvoie rien car il ne peut pas calculer la moyenne
 
@@ -52,101 +60,55 @@ print(m)
 # Que se passe-t-il si on a un triplet qui n'est pas du tout une note ?
 # Cela ne fonctionne pas 
 
-
-#5
-
 class Note:
+  instances = []
   def __init__(self, eleve, matiere, valeur): #La méthode pour créer un objet
     self.eleve = eleve
     self.matiere = matiere
     self.valeur = valeur
+    self.instances.append(self)
 
-
-  def afficher(self):
-    print('eleve', self.eleve, 'matiere', self.matiere, 'note', self.valeur)
-
-
-onote = Note('eleve1', 'maths', 13)
-print(onote.eleve)
-print(onote.matiere)
-print(onote.valeur)
-Note.afficher(onote)
-
-class Note:
-  instances = []  
-  def __init__(self, eleve, matiere, valeur): #La méthode pour créer un objet
-    self.eleve = eleve
-    self.matiere = matiere
-    self.valeur = valeur
-
-    Note.instances.append(self)
 
   def __str__(self):
     return f"{self.__class__.__name__}('{self.eleve}','{self.matiere}','{self.valeur}')"
-
-
   @classmethod
+ 
+ 
   def vider(cls):
     cls.instances = []
-
+  
+  
+  def afficher(self):
+      print(self)
+ 
+ 
   @classmethod
-  def moyenne(cls):
-    return(sum(n.valeur for n in cls.instances)/len(cls.instances))
+  def moyenne(cls, eleve=None, matiere=None):
+      print('****', cls.instances)
+      moyenne = [x for x in cls.instances if x.eleve == eleve] if eleve is not None else _notes
+      moyenne_matiere = [x for x in moyenne if x.matiere == matiere] if matiere is not None else moyenne
+      return sum([x.valeur for x in moyenne_matiere])/len(moyenne_matiere)
+
+
+onote = Note('eleve1', 'maths', 13)
+
+print(onote.matiere)
+
+print(onote.valeur)
+
+Note.afficher(onote)
 
 
 
+onotes=[Note(x,y,z) for x,y,z in notes]
 
+Note.moyenne('eleve2', 'math')
 
-if __name__ == '__main__':
-    note1 = ('eleve1', 'math', 13)
-    note2 = ('eleve1', 'physique', 15)
-    note3 = ('eleve1', 'math', 13)
-    note4 = ('eleve1', 'eco', 12)
-    note5 = ('eleve1', 'eco', 13)
-    note6 = ('eleve1', 'math', 12)
-    note7 = ('eleve2', 'math', 13)
-    note8 = ('eleve2', 'math', 14)
-
-
-    notes = [note1, note2, note3, note4, note5, note6,note7,note8]
-
-    notes_elv1 = [note for note in notes if note[0] == 'eleve1']
-
-    print(notes_elv1)
-
-    print(sum(note[2] for note in notes_elv1)/len(notes_elv1))
-
-    notes_elv1_math = [n for n in notes_elv1 if n[1] == 'math']
-
-    print(sum(n[2] for n in notes_elv1_math)/len(notes_elv1_math))
-
-
-    print(moyenne_tuple(notes, 'eleve1', 'math'))
-
-
-    onote = Note('eleve1', 'maths', 13)
+notes_enregistrées = []
 
 
 
-
-
-    onotes = [Note(eleve, matiere,  valeur) for eleve, matiere, valeur in notes]
-
-
-
-
-    onotes = [Note(*note) for note in notes]
-
-
-
-    Note.vider()
-
-
-
-    onotes = [Note(*note) for note in notes]
-
-    print(Note.moyenne()) 
-
-
-
-
+def moyenne_Note(notes, eleve = None, matiere = None):
+  moyenne = [x for x in notes if x.eleve == eleve] if eleve is not None else notes
+  moyenne_matiere = [x for x in moyenne if x.matiere == matiere] if matiere is not None else moyenne
+  return sum([x.valeur for x in moyenne_matiere])/len(moyenne_matiere)
